6ed7f38659a7af60417481db42ddcd5afd9037cc
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-07-30 18:54:01 +0530

    first

---
+++ 4_Binary search/12_Find the index of first 1 in an infinite sorted array of 0s and 1s.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int searchPos(vector<int>& nums, int target){
+        int start = 0;
+        int end = 1;
+        while (end < nums.size() && target > nums[end]) {
+            start = end;
+            end = end * 2;
+        }
+        if (end >= nums.size()) {
+            end = nums.size() - 1;
+        }
+        return search(nums, start, end, target);
+    }
+    int Binarysearch(vector<int>& nums,int start, int end int target) {
+        int start = 0, end = nums.size() - 1;
+        int result = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                if (findFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+};
