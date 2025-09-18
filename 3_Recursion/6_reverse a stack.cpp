432ad88e1f2bf9b9088273f00db4beb3649d694d
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-09-18 12:42:30 +0530

    commit

--- 3_Recursion/6_reverse a stack.cpp
+++ 3_Recursion/6_reverse a stack.cpp
@@ -1,2 +1,27 @@
 // practice link = https://www.geeksforgeeks.org/problems/reverse-a-stack/1
 
+class Solution {
+  public:
+    void reverseStack(stack<int> &st) {
+        // code here
+        if (st.size()== 1){
+            return;
+        }
+        int temp = st.top();
+        st.pop();
+        reverseStack(st);
+        insert(st,temp);
+        return;
+    }
+    void insert(stack<int> &st,int ele){
+        if (st.size()==0){
+            st.push(ele);
+            return;
+        }
+        int temp = st.top();
+        st.pop();
+        insert(st,ele);
+        st.push(temp);
+        return;
+    }
+};
