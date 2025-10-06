e03255bcb7df57dd0e37c083e4287c0dbaea2dc1
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-10-06 23:44:52 +0530

    string

---
+++ 11_String/2_Reverse Words in a String.cpp
@@ -0,0 +1,48 @@
+// problem link = https://leetcode.com/problems/reverse-words-in-a-string/
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans ="";
+        string temp = "";
+        stack<string> st;
+        for (int i=0;i<s.size();i++){
+            char ch = s[i];
+            if (ch ==' '){
+                if(!temp.empty()){
+                    st.push(temp);
+                }
+                temp="";
+            }
+            else{
+                temp+= ch;
+            }
+        }
+
+        if (!temp.empty()) {
+            st.push(temp);
+        }
+        while(!st.empty()){
+            ans += st.top();
+            st.pop(); 
+            if (!st.empty()) ans += " ";
+
+        }
+        return ans;
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stringstream ss(s);
+//         string word;
+//         vector<str> words
+//         if(ss >> word){
+//             words
+//         }
+//     }
+// };
