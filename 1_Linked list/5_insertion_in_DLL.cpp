5673994936403e536dedec5e35bf1a4a88a4cab0
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-10-12 14:50:18 +0530

    linked list

---
+++ 1_Linked list/5_insertion_in_DLL.cpp
@@ -0,0 +1,54 @@
+ 
+
+#include<iostream>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int val){
+        data = val;
+        next= NULL;
+        prev= NULL;
+    }
+};
+class DoublyList{
+    Node* head;
+    Node* tail;
+    public:
+    DoublyList(){
+        head = NULL;
+        tail = NULL;
+    }
+    void insertAtHaed(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+    void printlist(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<< temp->data << " <=> ";
+            temp = temp->next;
+        }
+        cout<< "NULL" << endl;
+    }
+};
+
+int main(){
+    DoublyList dll;
+    dll.printlist();
+    dll.insertAtHaed(10);
+    dll.insertAtHaed(20);
+    dll.insertAtHaed(30);
+    dll.printlist();
+    return 0;
+}
