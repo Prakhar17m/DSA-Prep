115a6d36207bfcb43d7b0b491caf71219e4fafe3
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-10-14 23:08:51 +0530

    linked list

---
+++ 1_Linked list/13_check if LL is palindrome or not.cpp
@@ -0,0 +1,49 @@
+// practice link = https://leetcode.com/problems/palindrome-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newNode = reverse(slow);
+        ListNode* first = head;
+        ListNode* second = newNode;
+        bool palindrome = true;
+        while (second != nullptr) {
+            if (first->val != second->val) {
+                palindrome = false;
+                break;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverse(newNode);
+        return palindrome;
+    }
+};
