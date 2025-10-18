d909632d286b14e9f000565b4bc5d299ead16c91
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-10-18 16:31:26 +0530

    linked list

--- 1_Linked list/20_Add one to a number represented by LL.cpp
+++ 1_Linked list/20_Add one to a number represented by LL.cpp
@@ -1,3 +1,44 @@
 //practice link = https://www.geeksforgeeks.org/problems/add-1-to-a-number-represented-as-linked-list/1
 
 
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    int helper(Node* temp){
+        if(temp ==NULL){
+            return 1;
+        }
+        int carry  = helper(temp->next);
+        temp->data = temp->data+carry;
+        if(temp->data < 10){
+            return 0;
+        }
+        temp->data = 0;
+        return 1;
+    }
+    Node* addOne(Node* head) {
+        // Your Code here
+        int carry = helper(head);
+        if(carry == 1){
+            Node* newnode = new Node(1);
+            newnode->next = head;
+            head= newnode;
+        }
+        return head;
+        // return head of list after adding one
+    }
+};
