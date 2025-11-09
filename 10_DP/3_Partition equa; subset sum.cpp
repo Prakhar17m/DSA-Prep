8d165c8e3c15b58fc2a1130501e1626e1bfda137
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-11-09 15:10:35 +0530

    dp

---
+++ 10_DP/3_Partition equa; subset sum.cpp
@@ -0,0 +1,35 @@
+// practice link = https://leetcode.com/problems/partition-equal-subset-sum/description/
+
+class Solution {
+public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<bool>> t(n + 1, vector<bool>(sum + 1, false));
+        for(int i=0;i<=n;i++){
+            t[i][0] = true;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j = 1;j<=sum ;j++){
+                if(arr[i-1]<=j){
+                    t[i][j] = (t[i-1][j-arr[i-1]]) || (t[i-1][j]) ;
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum =0;
+        for(int i =0;i<nums.size() ; i++){
+            sum += nums[i];
+        }
+        if(sum%2 !=0){
+            return false;
+        }
+        else{
+            return isSubsetSum(nums, sum/2);
+        }
+    }
+};
