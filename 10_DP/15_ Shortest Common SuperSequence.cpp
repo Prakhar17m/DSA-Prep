8074ae6f62ae5a43be106868347229853281c121
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-11-11 00:12:07 +0530

    dp

--- 10_DP/15_ Shortest Common SuperSequence.cpp
+++ 10_DP/15_ Shortest Common SuperSequence.cpp
@@ -1,1 +1,25 @@
-//practice link = 
+//practice link = https://www.geeksforgeeks.org/problems/shortest-common-supersequence0322/1
+//p2  = https://leetcode.com/problems/shortest-common-supersequence/description/
+
+
+class Solution {
+  public:
+    int minSuperSeq(string &s1, string &s2) {
+        // code here
+        int n = s2.size();
+        int m = s1.size();
+        vector<vector<int>> t(m+1,vector<int> (n+1,0));
+        for(int i = 1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1] == s2[j-1]){
+                    t[i][j] = 1+ t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        return m+n-t[m][n];
+    }
+};
