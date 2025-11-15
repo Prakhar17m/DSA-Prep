b815b09f49b17850874ec7febf40da1682bebc1f
Prakhar Mehrotra <131417300+Prakhar17m@users.noreply.github.com>
2025-11-15 23:41:20 +0530

    dp

--- 14_Greedy/2_Fractional Knapsack.cpp
+++ 14_Greedy/2_Fractional Knapsack.cpp
@@ -1,3 +1,44 @@
 //practice link = https://www.geeksforgeeks.org/problems/fractional-knapsack-1587115620/1
 
 
+class Solution {
+public:
+    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
+        int n = val.size();
+        
+        // Vector to store (value/weight ratio, weight, value)
+        vector<pair<double, pair<int, int>>> items;
+        
+        for (int i = 0; i < n; i++) {
+            double ratio = (double)val[i] / wt[i];
+            items.push_back({ratio, {wt[i], val[i]}});
+        }
+        
+        // Sort items by ratio in descending order
+        sort(items.begin(), items.end(), [](auto &a, auto &b){
+            return a.first > b.first;
+        });
+        
+        double maxValue = 0.0;
+        
+        for (auto &item : items) {
+            double ratio = item.first;
+            int weight = item.second.first;
+            int value = item.second.second;
+            
+            if (capacity == 0) break;
+            
+            if (weight <= capacity) {
+                // Take the whole item
+                capacity -= weight;
+                maxValue += value;
+            } else {
+                // Take fractional part
+                maxValue += ratio * capacity;
+                capacity = 0;
+            }
+        }
+        
+        return maxValue;
+    }
+};
